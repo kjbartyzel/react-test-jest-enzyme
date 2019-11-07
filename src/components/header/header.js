2ae26981b53c6089f21fe8da7d16840ce0dd9b82
kjbartyzel <kjbartyzel@users.noreply.github.com>
2019-11-07 13:51:22 +0100

    header component and test

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,10 +4,10 @@ import Logo from './../../assets/graphics/logo.png'
 
 const Header = (props) => {
     return (
-        <header>
+        <header data-test='headerComponent'>
             <div className='wrap'>
                 <div className='logo'>
-                    <img src={Logo} alt='logo' title='logo' />
+                    <img className='logoImg' data-test='logoImg' src={Logo} alt='logo' title='logo' />
                 </div>
             </div>
         </header>
