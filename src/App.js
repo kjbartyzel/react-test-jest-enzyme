76e6deec0bedbfa80a80756f4e1a3b066f38d52e
kjbartyzel <kjbartyzel@users.noreply.github.com>
2020-02-16 18:36:15 +0100

    Add tests for methods

--- src/App.js
+++ src/App.js
@@ -15,22 +15,42 @@ const tempArray = [
     age: 24,
     onlineStatus: true
   }
-]
+];
+
+const initialState = {
+  hideBtn: false
+};
 
 class App extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      ...initialState
+    };
     this.fetch = this.fetch.bind(this);
   }
 
   fetch = () => {
     this.props.fetchPosts();
+    this.exampleMethod_updatesState();
+  };
+
+  exampleMethod_updatesState = () => {
+    const { hideBtn } = this.state;
+
+    this.setState({
+      hideBtn: !hideBtn
+    });
+  };
+
+  exampleMethod_returnsAValue = (number) => {
+    return number + 1;
   };
 
   render() {
-
     const { posts } = this.props;
+    const { hideBtn } = this.state;
     const configButton = {
       buttonText: 'Get posts',
       emitEvent: this.fetch
@@ -41,7 +61,10 @@ class App extends Component {
         <Header />
         <section className='main'>
           <Headline header='Posts' desc='Click the button to render posts' tempArr={tempArray} />
-          <SharedButton {...configButton} />
+
+          {!hideBtn &&
+            <SharedButton {...configButton} />
+          }
 
           {posts.length > 0 &&
             <div>
